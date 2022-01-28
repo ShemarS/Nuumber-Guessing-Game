b9e8b40bb95432640345278812783212106cb3d4
ShemarS <42276240+ShemarS@users.noreply.github.com>
2022-01-27 22:06:00 -0500

    fixed UI and created generate function

--- app/src/main/java/com/example/numberguessinggame/MainActivity.kt
+++ app/src/main/java/com/example/numberguessinggame/MainActivity.kt
@@ -2,14 +2,38 @@ package com.example.numberguessinggame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    var tries = 0
+    val LIMIT = 100
+    var correctNumber = generateRandom()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        triesTextView.text = "Tries: $tries"
     }
 
-    fun testFunction() {
-        val hmm = "hopefully this works now :/ "
+    private fun generateRandom(): Int {
+        val correctNumber = Random.nextInt(LIMIT)
+
+        return correctNumber
     }
+
+    fun guess(view: View) {
+        var guessedNumber = userInputTextNumber.text
+        tries++
+        triesTextView.text = "Tries: $tries"
+
+        textView.text = correctNumber.toString()
+
+    }
+
+
 }
